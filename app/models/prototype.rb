6f0505294385b031352da115e5806cde851ee62c
masarusiro <huaz4906@gmail.com>
2023-04-06 00:20:19 +0900

    バリデーションの設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,9 @@
 class Prototype < ApplicationRecord
   belong_to :user
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
 end
