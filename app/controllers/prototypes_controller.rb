018f1719ca46f8c906225a26d7d05ace63ccdfd0
masarusiro <huaz4906@gmail.com>
2023-04-07 23:20:59 +0900

    ログインしていないユーザーのページ遷移を制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :new]
+
   def index
     @prototypes = Prototype.all
   end
