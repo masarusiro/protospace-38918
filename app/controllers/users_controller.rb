d0f79716dde0d62a61e3f43becaaacc5f20b8fbb
masarusiro <huaz4906@gmail.com>
2023-04-07 22:54:24 +0900

    ユーザー詳細ページでユーザー情報を見れるようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
  
   def show
+    @user = User.find(params[:id])
+    @name = current_user.name
+    @profile = current_user.profile
+    @occupation = current_user.occupation
+    @position = current_user.position
+    @prototypes = current_user.prototypes
+
 
   end
 
